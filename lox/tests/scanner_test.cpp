e6e2480a4d51e827a3d595f3ddc99374e843f850
Jacob Wujciak-Jens <jacob@wujciak.de>
2024-06-25 06:29:28 +0200

    rework scanner tests

--- lox/tests/scanner_test.cpp
+++ lox/tests/scanner_test.cpp
@@ -5,11 +5,71 @@
 
 #include "lox/scanner.hpp"
 
+#include <algorithm>
 #include <cctype>
+#include <cstdlib>
+#include <limits>
 #include <locale>
+#include <numeric>
 #include <vector>
 
 #include <catch2/catch_test_macros.hpp>
+#include <fmt/core.h>
+
+static const std::map<Token::Type, std::string_view> simple_tokens{
+    {Token::Type::LEFT_PAREN, "("},     {Token::Type::RIGHT_PAREN, ")"},
+    {Token::Type::LEFT_BRACE, "{"},     {Token::Type::RIGHT_BRACE, "}"},
+    {Token::Type::COMMA, ","},          {Token::Type::DOT, "."},
+    {Token::Type::MINUS, "-"},          {Token::Type::PLUS, "+"},
+    {Token::Type::SEMICOLON, ";"},      {Token::Type::SLASH, "/"},
+    {Token::Type::STAR, "*"},           {Token::Type::BANG, "!"},
+    {Token::Type::BANG_EQUAL, "!="},    {Token::Type::EQUAL, "="},
+    {Token::Type::EQUAL_EQUAL, "=="},   {Token::Type::GREATER, ">"},
+    {Token::Type::GREATER_EQUAL, ">="}, {Token::Type::LESS, "<"},
+    {Token::Type::LESS_EQUAL, "<="},    {Token::Type::AND, "and"},
+    {Token::Type::CLASS, "class"},      {Token::Type::ELSE, "else"},
+    {Token::Type::FALSE, "false"},      {Token::Type::FUN, "fun"},
+    {Token::Type::FOR, "for"},          {Token::Type::IF, "if"},
+    {Token::Type::NIL, "nil"},          {Token::Type::OR, "or"},
+    {Token::Type::PRINT, "print"},      {Token::Type::RETURN, "return"},
+    {Token::Type::SUPER, "super"},      {Token::Type::THIS, "this"},
+    {Token::Type::TRUE, "true"},        {Token::Type::VAR, "var"},
+    {Token::Type::WHILE, "while"}};
+
+TEST_CASE("Simple tokens", "[scanner]") {
+  for (auto const [type, lexem] : simple_tokens) {
+    std::string input = fmt::format("{}", lexem);
+    Scanner scanner{input};
+    std::vector tokens = scanner.scanTokens();
+    REQUIRE(not scanner.hasError());
+    REQUIRE(tokens.size() == 2);
+    REQUIRE(tokens.front().type == type);
+    REQUIRE(tokens.front().lexem == lexem);
+    REQUIRE(tokens.front().line == 1);
+  }
+
+  SECTION("unexpected character error") {
+    Scanner unexpected{"!(\n)@#"};
+    auto empty = unexpected.scanTokens();
+    REQUIRE(unexpected.hasError());
+    REQUIRE(unexpected.getErrors().size() == 2);
+    REQUIRE(unexpected.getErrors().at(0).message ==
+            "Unexpected character '@'.");
+  }
+}
+
+TEST_CASE("Simple tokens with whitespace", "[scanner]") {
+  for (auto const [type, lexem] : simple_tokens) {
+    std::string input = fmt::format("\n\t {} \t\n", lexem);
+    Scanner scanner{input};
+    std::vector tokens = scanner.scanTokens();
+    REQUIRE(not scanner.hasError());
+    REQUIRE(tokens.size() == 2);
+    REQUIRE(tokens.front().type == type);
+    REQUIRE(tokens.front().lexem == lexem);
+    REQUIRE(tokens.front().line == 2);
+  }
+}
 
 TEST_CASE("Multichar tokens are lexed correctly", "[scanner]") {
   Scanner scanner{
@@ -29,21 +89,14 @@ TEST_CASE("Multichar tokens are lexed correctly", "[scanner]") {
     REQUIRE(tokens.at(i).type == expected.at(i));
   }
 
-  SECTION("unexpected character error") {
-    Scanner unexpected{"!(\n)@"};
-    auto empty = unexpected.scanTokens();
-    REQUIRE(unexpected.hasError());
-    REQUIRE((unexpected.hasError() ? unexpected.getErrors().at(0).message
-                                   : "") == "Unexpected character '@'.");
-  }
 }
 
-TEST_CASE("Scanner handles empty input", "[scanner]"){
-  Scanner scanner {""};
+TEST_CASE("Scanner handles empty input", "[scanner]") {
+  Scanner scanner{""};
   std::vector tokens = scanner.scanTokens();
   REQUIRE(not scanner.hasError());
   REQUIRE(tokens.size() == 1);
-  REQUIRE(tokens.at(0).type == Token::Type::END_OF_FILE );
+  REQUIRE(tokens.at(0).type == Token::Type::END_OF_FILE);
 }
 
 TEST_CASE("String literals", "[scanner]") {
@@ -89,10 +142,11 @@ TEST_CASE("Identifiers", "[scanner]") {
 
 TEST_CASE("Keywords", "[scanner]") {
   Scanner scanner{"if(orchid or ifrit)"};
-  std::vector<Token::Type> expected{Token::Type::IF,         Token::Type::LEFT_PAREN,
-                                  Token::Type::IDENTIFIER, Token::Type::OR,
-                                  Token::Type::IDENTIFIER, Token::Type::RIGHT_PAREN,
-                                  Token::Type::END_OF_FILE};
+  std::vector<Token::Type> expected{
+      Token::Type::IF,         Token::Type::LEFT_PAREN,
+      Token::Type::IDENTIFIER, Token::Type::OR,
+      Token::Type::IDENTIFIER, Token::Type::RIGHT_PAREN,
+      Token::Type::END_OF_FILE};
   auto tokens = scanner.scanTokens();
 
   REQUIRE(not scanner.hasError());
@@ -109,10 +163,10 @@ TEST_CASE("Multi-line block comments work", "[scanner]") {
   Scanner scanner{
       "\n  /*if(orchid \nor// \"ifrit)\n*/ if(orchid or ifrit) // more "
       "comments!\n/* single line block comment */ or"};
-  std::vector<Token::Type> expected{Token::Type::IF,         Token::Type::LEFT_PAREN,
-                                  Token::Type::IDENTIFIER, Token::Type::OR,
-                                  Token::Type::IDENTIFIER, Token::Type::RIGHT_PAREN,
-                                  Token::Type::OR,         Token::Type::END_OF_FILE};
+  std::vector<Token::Type> expected{
+      Token::Type::IF, Token::Type::LEFT_PAREN, Token::Type::IDENTIFIER,
+      Token::Type::OR, Token::Type::IDENTIFIER, Token::Type::RIGHT_PAREN,
+      Token::Type::OR, Token::Type::END_OF_FILE};
   auto tokens = scanner.scanTokens();
 
   REQUIRE(not scanner.hasError());
@@ -129,3 +183,4 @@ TEST_CASE("Multi-line block comments work", "[scanner]") {
   REQUIRE((unterminated.hasError() ? unterminated.getErrors().at(0).message
                                    : "") == "Unterminated block comment.");
 }
+
